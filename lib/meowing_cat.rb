2fbe7d6935f30613bbec55f793b3d7ff5e16e051
Nick Nobile <nickcnobile@gmail.com>
2020-09-25 04:23:41 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,8 +1,9 @@
 class Cat
-  attr_accessor :name, :meow
+  attr_accessor :name
+
+def meow
+  puts "meow!"
+end  
 end
 
-maru = Cat.new
-maru.name = "Maru"
-meowing_cat = Cat.new
-meowing_cat.meow = "meow!"
+
